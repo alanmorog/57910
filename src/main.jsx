4b3821c36a64d5878a1fedaca0d712effce24b87
Alanmoro <alan.gabriel.moro@gmail.com>
2023-12-25 15:42:24 -0300

    Initial Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,10 @@
-//1. importar librerias
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-import React from "react";
-import ReactDOM from "react-dom";
-
-
-//2. obtener la referencia al elemento donde ese va a renderizar la app
-
-const rootDivElement = document.getElementById('root');
-
-//3. le decimos a react que tome el control de ese elemto\
-//ese elemento va a ser la razin de nuestra app
-
-const root = ReactDOM.createRoot(rootDivElement);
-
-//4. creasr un componente que se va a renderizxar / mostrar el elemento rainz
-
-
-function App() {
-    return <h1>Hola Mundo</h1>;
-}
-
-//5. renderizar el componente en el elemento rainz
-
-
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
