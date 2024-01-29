5d68fd6a050b5696822053ea3374780a67f7ac9a
Alanmoro <alan.gabriel.moro@gmail.com>
2024-01-28 22:54:54 -0300

    add lista de productos

---
+++ src/utils/MockData.js
@@ -0,0 +1,68 @@
+export const products = [
+    {
+        id: 1,
+        nombre: "Fender",
+        modelo: "Stratocaster",
+        description:
+            "Guitarra fender modelo stratocaster diseñada con madera de caoba ensamblada en California",
+        precio: 30000,
+        stock: 2,
+        image: "https://mariogomez.com.ar/wp-content/uploads/2023/12/fender-stratocaster-eric-johnson-s-principal-600x600.jpg",
+        tipo: "guitarra"
+    },
+    {
+        id: 2,
+        nombre: "Fender",
+        modelo: "Squier",
+        description:
+            "Guitarra fender modelo stratocaster diseñada con madera de caoba ensamblada en California",
+        precio: 15000,
+        stock: 3,
+        image: "https://www.miche.com.co/cdn/shop/products/GUITARRAELECTRICAFENDERSQUIER40ANIVERSARIOSTRATOCASTEREDICIONDORADALRLSSBMOD0379410547DIAPSONENMAPLEMICHETIENDAGUI960-3_809x894.jpg?v=1667595270",
+        tipo: "guitarra"
+    },
+    {
+        id: 3,
+        nombre: "Gibson",
+        modelo: "Les Paul",
+        description:
+            "La guitarra eléctrica Gibson Les Paul Standard 2018 en mojave burst brillante es la elección perfecta para los amantes de la música que buscan un instrumento de alta calidad y diseño clásico.",
+        precio: 15000,
+        stock: 1,
+        image: "https://media.guitarcenter.com/is/image/MMGS7/L54489000002000-00-720x720.jpg",
+        tipo: "guitarra"
+    },
+    {
+        id: 4,
+        nombre: "Ibanez",
+        modelo: "Js Series Js3",
+        description:
+            "",
+        precio: 15000,
+        stock: 5,
+        image: "https://http2.mlstatic.com/D_NQ_NP_660236-MLA31595237710_072019-O.webp",
+        tipo: "bajo"
+    },
+    {
+        id: 5,
+        nombre: "Natal",
+        modelo: "Natal5",
+        description:
+            "La batería NATAL DNA viene completa, con hardware, platillos, pedal para el bombo, butaca e incluso baquetas",
+        precio: 55000,
+        stock: 3,
+        image: "https://http2.mlstatic.com/D_NQ_NP_712859-MLA71414454611_082023-O.webp",
+        tipo: "bateria"
+    },
+    {
+        id: 6,
+        nombre: "Yamaha",
+        modelo: "PRS Series",
+        description:
+            "Yamaha es una marca que fabrica instrumentos de alta calidad combinando lo artesanal y tradicional con la última tecnología digital.",
+        precio: 15000,
+        stock: 3,
+        image: "https://http2.mlstatic.com/D_NQ_NP_659504-MLA46440505116_062021-O.webp",
+        tipo: "teclado"
+    },
+]
