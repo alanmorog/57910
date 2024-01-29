ccaaa77ac64427bdf02336d33a66fcfebc18c4e7
Alanmoro <alan.gabriel.moro@gmail.com>
2024-01-28 22:55:01 -0300

    add contact page

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Dashboard from '../../components/Dashboard/Dashboard'
+
+const Contact = () => {
+  return (
+    <Dashboard>
+      <div>pagina Contacto</div>
+    </Dashboard>
+  )
+}
+
+export default Contact
