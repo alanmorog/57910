1e35db122f9a41ea7113666c5bd8cae1e2d57da0
Alanmoro <alan.gabriel.moro@gmail.com>
2024-01-28 22:55:23 -0300

    add logo en navBar

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import styles from "./Logo.module.scss"
+
+function Logo() {
+    return (
+        <>
+        <div className={styles.logo}><p>MusicSHOP</p></div>
+        </>
+    )
+}
+
+
+
+export default Logo
