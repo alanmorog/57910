96ac2c4ba28186ea76f71e8551cfe516e8cc23f2
Alanmoro <alan.gabriel.moro@gmail.com>
2024-01-30 01:27:25 -0300

    add router al dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import NavBar from "../NavBar/NavBar"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import PageNotFound from '../PageNotFound/PageNotFound'
+
 
 
 const Dashboard = ({children}) => {
     return (
-        <>
+        <BrowserRouter>
             <NavBar />
-            <div>{children}</div>
-        </>
+            <Routes>
+                <Route path="/" element={  children }/>
+                <Route path="/products" element={  children }/>
+                <Route path="/products/:categoryId" element={  children }/>
+                <Route path="*" element={  <PageNotFound/> }/>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
