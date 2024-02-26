beb41e35ed5df30261b6d3c48449156ccd4ac5cc
Alanmoro <alan.gabriel.moro@gmail.com>
2024-02-26 00:34:00 -0300

    createCart

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,15 @@
+import styles from "./Cart.module.scss"
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import React from 'react'
+
+const Cart = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Cart
